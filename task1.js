9a04a5cc989ffd5cdc5a28c2197eb0af13a78424
Federica Costantino <ricacostt@gmail.com>
2022-11-08 12:15:59 +0000

    task 1 done

--- task1.js
+++ task1.js
@@ -1,3 +1,9 @@
 const cats = ["tony", "daisy", "socks", "rockie" ];
 
-const capitalisedCats = // your code here
+const capitalisedCats = cats.map(upperCase)
+
+function upperCase(cat){
+return cat.toUpperCase()
+}
+console.log(cats)
+console.log(capitalisedCats)
